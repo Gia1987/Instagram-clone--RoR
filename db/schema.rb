67c83480027ba12825a47a79a08316245a9fbfeb
Gia1987 <giacomo.n27@gmail.com>
2018-06-19 11:40:16 +0100

    User can create post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_19_081224) do
+ActiveRecord::Schema.define(version: 2018_06_19_094709) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
